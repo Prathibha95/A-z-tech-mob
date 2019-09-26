0dd47daa00e629bd6a9d671375302f8cac4ba84f
unknown <prathibha.17@itfaq.mrt.ac.lk>
2019-09-26 18:46:26 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'ideapool', loadChildren: './ideapool/ideapool.module#IdeapoolPageModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/ideapool/ideapool.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ideapool',
+  templateUrl: './ideapool.page.html',
+  styleUrls: ['./ideapool.page.scss'],
+})
+export class IdeapoolPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
